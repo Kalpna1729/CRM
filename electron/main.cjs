8af1dc759a803548bafc59cc0b056297230bcfdc
gulshankr1729 <gk182426@gmail.com>
2026-05-13 11:24:00 +0530

    v1.0.11

--- electron/main.cjs
+++ electron/main.cjs
@@ -1,3 +1,66 @@
+// const { app, BrowserWindow, dialog } = require('electron');
+// const { autoUpdater } = require('electron-updater');
+// const path = require('path');
+
+// function createWindow() {
+//   const win = new BrowserWindow({
+//     width: 1400,
+//     height: 900,
+//     minWidth: 1024,
+//     minHeight: 700,
+//     webPreferences: {
+//       nodeIntegration: false,
+//       contextIsolation: true,
+//     },
+//     title: 'Grow Lotus CRM',
+//     autoHideMenuBar: true,
+//   });
+
+//   win.loadFile(path.join(__dirname, '../dist/index.html'));
+
+//   autoUpdater.setFeedURL({
+//     provider: 'github',
+//     owner: 'Kalpna1729',
+//     repo: 'CRM',
+//     token: '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'
+//   });
+
+//   autoUpdater.checkForUpdates();
+
+//   autoUpdater.on('update-available', () => {
+//     dialog.showMessageBox({
+//       type: 'info',
+//       title: 'Update Available',
+//       message: 'Naya update mil gaya! Download ho raha hai...',
+//       buttons: ['OK']
+//     });
+//   });
+
+//   autoUpdater.on('error', (err) => {
+//     dialog.showMessageBox({
+//       type: 'error',
+//       title: 'Update Error',
+//       message: err.message || err.toString(),
+//       buttons: ['OK']
+//     });
+//   });
+
+//   autoUpdater.on('error', (err) => {
+//     console.log('Update error:', err);
+//   });
+// }
+
+// app.whenReady().then(createWindow);
+
+// app.on('window-all-closed', () => {
+//   if (process.platform !== 'darwin') app.quit();
+// });
+
+// app.on('activate', () => {
+//   if (BrowserWindow.getAllWindows().length === 0) createWindow();
+// });
+
+
 const { app, BrowserWindow, dialog } = require('electron');
 const { autoUpdater } = require('electron-updater');
 const path = require('path');
@@ -35,19 +98,6 @@ function createWindow() {
       buttons: ['OK']
     });
   });
-
-  // autoUpdater.on('update-downloaded', () => {
-  //   dialog.showMessageBox({
-  //     type: 'question',
-  //     title: 'Update Ready',
-  //     message: 'Update download ho gaya! Abhi install karein?',
-  //     buttons: ['Haan, Install Karo', 'Baad Mein']
-  //   }).then(result => {
-  //     if (result.response === 0) {
-  //       autoUpdater.quitAndInstall();
-  //     }
-  //   });
-  // });
 
   autoUpdater.on('error', (err) => {
     dialog.showMessageBox({
